5f929c5f82318a8d4d82cfae90adfab17d775c61
Swapnil <swapnil@abymtechnology.com>
2025-12-23 16:38:04 +0530

    data types

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,38 @@
+"Use strict";  // treat all the js code asa newer 
+// alert (3+3)   ----we are using node js not browser this we can do it 
+
+console.log(3
+    +3
+    );//code readbility should be high means it should be written in right structure 
+
+// TYPE  of data types:-their two  types ofdata types 
+
+// primitive data  types:-it storessimple and single values 
+
+
+//number -2  to power 53
+//biginit - big number used in stockmarker 
+//string - it can be written single quotes or double quotes '' or ""
+//boolean- the values which is true or false 
+//null-standalone values 
+//undeified -means still the value is not given and is empty 
+//symbol- unique
+
+
+
+
+
+
+// non -primitive data  types :-it stores complex and multiple values :-
+
+//object -
+let age = 30;
+NAME="swapnil";
+
+console.log(typeof age);// its type will be number 
+console.log(typeof null);//its 
+console.log(typeof NAME);
+console.log(typeof undefined);i
+
+
+
--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,19 @@
-console.log("swapnil");
+// let ,var and const :-
+const accountid = 8400822170;
+let username = "swapnil";
+var cityname = "Raebareli";
+bankname = "hdfc";// without using keywords we can declare a varaiable 
+
+// accountid =7884784; it means const can not be chnaged 
+username ="rahul";
+cityname="lucknow";
+bankname="sbi";
+
+
+let accounttype;// in javascripit if we do give any in a variable it will show automatically undefined
+
+console.log(accountid);
+console.table([username,cityname,bankname,accounttype])// it means let and var can be changed 
+
+// prefer not to use var due issue in block scope and functional scope 
+
